f997f5a973e2ff6ad6535d163d10f791153d44fa
John McCann Cunniff Jr <john.cunniff@voladynamics.com>
2022-05-17 15:33:39 -0400

    CHG rename package name

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 from setuptools import setup
 
 # Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
-setup(name="Flask-SQLAlchemy", install_requires=["Flask>=1.0.4", "SQLAlchemy>=1.2"])
+setup(name="FastAPI-SQLAlchemy", install_requires=["fastapi", "SQLAlchemy>=1.2"])
